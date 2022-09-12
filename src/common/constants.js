643be5b972f61ae314f819abcb20397ec6f2d49b
Taratora117 <alex.j.hkrystev@gmail.com>
2022-09-12 09:46:43 +0200

    constants + main loop

---
+++ src/common/constants.js
@@ -0,0 +1,7 @@
+const SIZE = 800;
+const FPS = 20000;
+
+export {
+  SIZE,
+  FPS
+};
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import {
+  SIZE,
+  FPS,
+} from './common/constants.js';
+import spiral from './helpers/spiral.js';
+import prime from './helpers/prime.js';
+
+// init
+document.getElementById('root').innerHTML = `<canvas id="myCanvas" height="${SIZE}" width="${SIZE}" style="background-color: black;"></canvas>`;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = 'white';
+// ctx.strokeStyle = 'white';
+
+// TODO make more abstract
+const matrix = spiral(SIZE, 100);
+let n = 1;
+
+function animate() {
+
+  if (n <= 10000) {
+    if (prime(n)) {
+      ctx.fillStyle = 'black';
+    } else {
+      ctx.fillStyle = 'white';
+    };
+    ctx.fillRect(matrix[n][0], matrix[n][1], SIZE / 100, SIZE / 100)
+    n++;
+  }
+
+  setTimeout(() => {
+    window.requestAnimationFrame(animate);
+  }, 1000 / FPS);
+};
+animate();
